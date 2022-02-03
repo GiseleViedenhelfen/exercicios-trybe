487ad3f5e34ed0d2708036bbe8051146053a12f5
Gisele <gisele.viedenhelfen@gmail.com>
2022-02-03 16:20:11 -0300

    segundo exercicio logica

---
+++ bloco-4/logica/ex2.js
@@ -0,0 +1,12 @@
+/*2- Agora, desenvolva um algoritmo que é capaz de inverter uma palavra. 
+Por exemplo, a palavra "banana" seria invertida para "ananab". 
+Utilize a string abaixo como exemplo, 
+depois troque por outras para verificar se 
+seu algoritmo está funcionando corretamente.*/
+let word = 'amor';
+
+
+
+for(let index=word.length; index>=1;index-=1){
+        console.log(word[index-1])
+}
