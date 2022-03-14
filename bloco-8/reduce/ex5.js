cb9a22264a8eea975de8f2e908dd47b348180ee4
Gisele <gisele.viedenhelfen@gmail.com>
2022-03-14 18:46:11 -0300

    exercicio 5

---
+++ bloco-8/reduce/ex5.js
@@ -0,0 +1,15 @@
+const names = [
+  'Aanemarie', 'Adervandes', 'Akifusa',
+  'Abegildo', 'Adicellia', 'Aladonata',
+  'Abeladerco', 'Adieidy', 'Alarucha',
+];
+
+function containsA(array) {
+  return array.reduce((contador, valor)=> 
+  contador += valor.split('').reduce((contador, valor) =>{
+    if(valor.toLowerCase() ==='a') return contador += 1
+    return contador
+  },0),0)
+}
+
+console.log(containsA(names));
